b18038aac5b518ba77bd6ac66d2e9e4627259abd
nielinjie <nielinjie@gmail.com>
2020-05-23 14:55:03 +0800

    比较依赖版本加上等于

--- src/Dep.ts
+++ src/Dep.ts
@@ -2,6 +2,7 @@ import * as fs from "fs-extra";
 import { Project, projects } from "./Project";
 import * as path from "path";
 import * as semver from "semver";
+import { fail } from "assert";
 
 export function dependencePath(project: Project, dependence: string): string {
   return path.resolve(project.path, "node_modules", dependence);
@@ -27,6 +28,14 @@ export class Dependency {
       (project) => project.name === this.name
     );
     const depVersion = findProject?.version;
-    return depVersion !== undefined && semver.gtr(depVersion, this.version);
+    return (
+      depVersion !== undefined &&
+      semver.gte(depVersion, getRangeVersion(this.version))
+    );
   }
+}
+
+function getRangeVersion(range: string): string {
+  if (range.startsWith("^") || range.startsWith("~")) return range.substring(1);
+  else fail("only support range started with ~/^");
 }
